5e485da30cec9a1bf72c97b9df43025391a8a76f
androidm16 <mkhosanasiyabonga@gmail.com>
2022-05-12 16:04:39 +0200

    installed cors

--- server.js
+++ server.js
@@ -1,27 +1,28 @@
 const express = require ('express');
-const dotenv = require ('dotenv');
 const colors = require ('colors');
 const morgan = require ('morgan');
 const path = require('path');
 const todoRoute = require('./routes/todo');
-
+require('dotenv').config()
+const Cors = require('cors')
 const connectDB = require('./config/db');
-dotenv.config({ path: './.env' })
+// dotenv.config()
 connectDB()
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
+app.use(Cors())
 
 if(process.env.NODE === 'development'){
     app.use(morgan('dev'))
 }
 
-const PORT = process.env.PORT || 4500;
+const PORT = process.env.PORT;
 
 app.use('/api/task', todoRoute)
 
 app.get('/', (req, res) => {
-    res.send('API is running good')
+    res.json('API is running good')
 })
 
 app.listen(PORT, console.log(`server is running on port ${PORT}`.yellow.bold))
